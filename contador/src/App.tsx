2fe95fbddae679491353adc27f41a6da2dd4eb2c
Diego ROman <diegoroman108@gmail.com>
2025-01-14 11:35:23 -0300

    counter+diego

---
+++ contador/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ClickCounter from './counter';
+import Itemcount from './itemCount';
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: 'center' }}>Contador de Cliques</h1>
+      <ClickCounter />
+    <Itemcount />
+    </div>
+  );
+}
+
+export default App;
